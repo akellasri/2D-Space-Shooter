20e179eef757d4fba18e4e2537735be80b6feca2
nonokeks <n.grosch@gmx.de>
2021-02-10 16:27:32 +0100

    added laserbullet and scripts, currently compiling errors

---
+++ Space-Shooter/Assets/Scripts/LaserController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+	private Transform laser;
+	private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = GetComponent<Transform> ();
+    }
+    
+    // move at set period at time
+    void FixedUpdate()
+    {
+    	laser.position += Vector3.up * speed;
+
+    	if(laser.position.y >= 10)
+    		Destroy (gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == 'Enemy')
+        {
+        	Destroy (other.gameObject);
+        	Destroy (gameObject);
+        	// Increase Score?
+        }
+        else if (other.tag == 'Base')
+        	Destroy (gameObject);
+        // reaction to shield?
+        
+    }
+}
---
+++ Space-Shooter/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	private Transform player;
+	public float speed;
+	public float maxBound, minBound;
+
+	public GameObject shot; // Later drag laser into this but in Unity
+	public Transform shotSpwan; //Player Trasnform drag in Unity
+	public float fireRate;
+
+	private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform> ();
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (player.position.x < minBound && h < 0)
+        	h = 0;
+        else if (player.position.x > maxBound && h > 0)
+        	h = 0;
+
+        player.position += Vector3.right * h * speed;
+
+    }
+
+    void Update()
+    {
+    	if(Input.GetButton("Fire1") && Time.time > nextFire)
+    	{
+    		nextFire = Time.time + fireRate;
+    		Instantiate(shot, shotSpwan.position, shotSpwan.rotation);
+    	}
+    }
+}
