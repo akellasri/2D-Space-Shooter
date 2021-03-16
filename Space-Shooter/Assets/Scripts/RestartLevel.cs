706ba71981bdf3308eb6000eb8a78accb900648f
nonokeks <n.grosch@gmx.de>
2021-03-16 14:58:13 +0100

    added second level
    enter by pressing space, text overlay not done yet

--- Space-Shooter/Assets/Scripts/RestartLevel.cs
+++ Space-Shooter/Assets/Scripts/RestartLevel.cs
@@ -5,17 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class RestartLevel : MonoBehaviour
 {
+    /* void Start()
+    {
+        SceneManager.LoadScene("Scene_001");
+    }*/
 
+    private static float start = 0;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R)){
-        	PlayerScore.playerScore = 0;
-        	GameOver.isPlayerDead = false;
-        	Time.timeScale = 1;
-            ShieldControl.hit = 0;
+        if(start == 0){
+            SceneManager.LoadScene("Scene_001");
+            start = 1;
+        }
 
-        	SceneManager.LoadScene("Scene_001");
+        if(Input.GetKeyDown(KeyCode.R)){
+            PlayerScore.playerScore = 0;
+            GameOver.isPlayerDead = false;
+            Time.timeScale = 1;
+            ShieldControl.hit = 0;
+            PlayerControl.health = 3;
+            EnemyControl.counter = 0;
+
+            SceneManager.LoadScene("Scene_001");
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            PlayerScore.playerScore = 0;
+            GameOver.isPlayerDead = false;
+            Time.timeScale = 1;
+            ShieldControl.hit = 0;
+            PlayerControl.health = 3;
+            EnemyControl.counter = 0;
+
+        	SceneManager.LoadScene("Scene_002");
         }
     }
 }
