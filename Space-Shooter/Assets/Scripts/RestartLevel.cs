afeae7e1a137ff53d0e581f5588beb64fb634ef9
nonokeks <n.grosch@gmx.de>
2021-03-30 12:09:01 +0200

    added start screen for level

--- Space-Shooter/Assets/Scripts/RestartLevel.cs
+++ Space-Shooter/Assets/Scripts/RestartLevel.cs
@@ -5,20 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class RestartLevel : MonoBehaviour
 {
-    /* void Start()
-    {
-        SceneManager.LoadScene("Scene_001");
-    }*/
+    private static float level = 1;
 
-    private static float start = 0;
     // Update is called once per frame
     void Update()
     {
-        if(start == 0){
-            SceneManager.LoadScene("Scene_001");
-            start = 1;
-        }
-
         if(Input.GetKeyDown(KeyCode.R)){
             PlayerScore.playerScore = 0;
             GameOver.isPlayerDead = false;
@@ -26,8 +17,10 @@ public class RestartLevel : MonoBehaviour
             ShieldControl.hit = 0;
             PlayerControl.health = 3;
             EnemyControl.counter = 0;
-
-            SceneManager.LoadScene("Scene_001");
+            if (level == 1)
+                SceneManager.LoadScene("Scene_001");
+            if (level == 2)
+                SceneManager.LoadScene("Scene_002");
         }
         if(Input.GetKeyDown(KeyCode.Space)){
             PlayerScore.playerScore = 0;
@@ -38,6 +31,7 @@ public class RestartLevel : MonoBehaviour
             EnemyControl.counter = 0;
 
         	SceneManager.LoadScene("Scene_002");
+            level = 2;
         }
     }
 }
---
+++ Space-Shooter/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartLevel : MonoBehaviour
+{
+	private Text start;
+	private static float started = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = GetComponent<Text>();
+        start.enabled = true;
+        Time.timeScale = 0;
+        if (started == 0){
+     		started = 1;   	
+        	SceneManager.LoadScene("Scene_000");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+        	SceneManager.LoadScene("Scene_001");
+        	start.enabled = false;
+        	Time.timeScale = 1;
+        }
+    }
+}
